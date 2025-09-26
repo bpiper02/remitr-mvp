af99395944bd1f26c8b800e233661a24a3b92319
bpiper02 <93992231+bpiper02@users.noreply.github.com>
2025-09-26 10:52:33 -0400

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["'DM Serif Display'", "serif"],
+        sans: ["'Inter'", "sans-serif"],
+      },
+      colors: {
+        brandGreen: "#1B5E20",
+        brandOrange: "#F57C00",
+      },
+    },
+  },
   plugins: [],
 };
+
